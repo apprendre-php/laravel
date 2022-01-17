249256cc013ed484d442f21d48e24a7489e0e8d6
William <work@suppo.fr>
2022-01-18 00:38:39 +0100

    Suppression des timestamps dans la table items.

--- database/migrations/2022_01_17_121354_create_items_table.php
+++ database/migrations/2022_01_17_121354_create_items_table.php
@@ -15,7 +15,6 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('name');
         });
     }
