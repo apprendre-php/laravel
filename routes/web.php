42d565aa6a6fa8869d0ba8e6244096c59788f71d
William <work@suppo.fr>
2022-01-18 11:26:02 +0100

    #4 Query Builder : Exercice 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// #4 Query Builder : Exercice 1
+
+Route::get('/guilds', function () {
+    $guilds = DB::table('guilds')->get();
+
+    echo '<ul>';
+    foreach ($guilds as $guild) {
+        echo "<li>" . $guild->name . "</li>";
+    }
+    echo '</ul>';
+});
+
+Route::get('/users-with-health-up-20', function () {
+    $users = DB::table('users')
+        ->where('health', '>', 20)
+        ->get();
+
+    echo '<ul>';
+    foreach ($users as $user) {
+        echo "<li>" . $user->name . "</li>";
+    }
+    echo '</ul>';
+});
+
+Route::get('/best-user', function () {
+    $user = DB::table('users')
+        ->orderByDesc('level')
+        ->limit(1)
+        ->first();
+
+    echo "<h1>" . $user->name . "</h1>";
+});
+
+Route::get('/users/{user}/items', function ($user) {
+    $userItems = DB::table('items')
+        ->leftJoin('item_user', 'item_user.item_id', '=', 'items.id')
+        ->where('item_user.user_id', $user)
+        ->get();
+
+    echo '<ul>';
+    foreach ($userItems as $item) {
+        echo "<li>" . $item->name . " / qté: " . $item->quantity . "</li>";
+    }
+    echo '</ul>';
 });
